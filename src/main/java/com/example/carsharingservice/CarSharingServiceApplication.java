cb19160a726c9fc66fbb31db5d36b22d3202afab
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-05-29 11:00:44 +0300

    init repo

---
+++ src/main/java/com/example/carsharingservice/CarSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.carsharingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carsharingservice/CarSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carsharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
