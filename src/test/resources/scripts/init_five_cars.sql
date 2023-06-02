3ecea313edbb5bd3da920f34b726e5b09216462b
potrap <potrapmax@gmail.com>
2023-06-02 16:36:33 +0300

    Added test resources

---
+++ src/test/resources/scripts/init_five_cars.sql
@@ -0,0 +1,5 @@
+INSERT INTO cars (model, brand, type, daily_fee) VALUES ("Model-5","Brand5","SEDAN",5);
+INSERT INTO cars (model, brand, type, daily_fee) VALUES ("Model-4","Brand","SEDAN",4);
+INSERT INTO cars (model, brand, type, daily_fee) VALUES ("Model-3","Brand","SEDAN",3);
+INSERT INTO cars (model, brand, type, daily_fee) VALUES ("Model-2","Brand","SEDAN",2);
+INSERT INTO cars (model, brand, type, daily_fee) VALUES ("Model","Brand","SEDAN",1);
